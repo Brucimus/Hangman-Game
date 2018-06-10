a337460dfc3b238924ff3e17e75e963c13fe2916
Brucimus <Brucimus@gmail.com>
2018-06-10 00:09:08 -0700

    random select GOT Names and add appropriate number of _s

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+var computerChoices = [["s","t","a","r","k"],["t","y","r","e","l","l"],["l","a","n","n","i","s","t","e","r"],["t","a","r","g","a","r","i","a","n"],["g","r","e","y","j","o","y"]]
+var win = 0;
+var losses = 0;
+var guesses = 12;
+var guessedLetters = "";
+
+document.onkeydown = function(Event) {
+    var userGuess = event.key;
+
+    var pickName = Math.floor(Math.random() * computerChoices.length);
+    
+    var nameBlanks = "";
+    for (var i = 0; i < computerChoices[pickName].length; i++)
+    {
+        nameBlanks = nameBlanks + "_ ";
+    }
+    var html = 
+        "<p>" + nameBlanks + "</p>";
+
+    document.querySelector("#game").innerHTML = html;
+}
