538c936d9626bec2afd66bced72f35c92986939d
Brucimus <Brucimus@gmail.com>
2018-06-10 18:54:09 -0700

    figured out letter and blank logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,23 +3,36 @@ var win = 0;
 var losses = 0;
 var guesses = 12;
 var guessedLetters = [];
+var nameBlanks = ""
+var pickName = -1;
+var guessedLetterInWord = false;
+var tempLetter = "";
 
 document.onkeydown = function(Event) {
     var userGuess = event.key;
 
-    var pickName = (Math.floor(Math.random() * computerChoices.length) - 1);
+    pickName =  2;//(Math.floor(Math.random() * computerChoices.length) - 1);
     
-    var nameBlanks = "";
+    nameBlanks = "";
+    
     guessedLetters.push(userGuess);
     for (var i = 0; i < computerChoices[pickName].length; i++)
     {
         for (var j = 0; j < guessedLetters.length; j++) {
-            if (guessedLetters[j] == computerChoices[pickName][i])
-            nameBlanks = nameBlanks + guessedLetters[j];
+            if (guessedLetters[j] === computerChoices[pickName][i]) {
+                guessedLetterInWord = true;
+                tempLetter = guessedLetters[j];
+                nameBlanks = nameBlanks + tempLetter + " ";
+                break;
+            }
             else {
-                nameBlanks = nameBlanks + "_ ";
+                guessedLetterInWord = false;
             }
         }
+        if (guessedLetterInWord === false) {
+            nameBlanks = nameBlanks + "_ ";
+            }
+
     }
     var html = 
         "<p>" + nameBlanks + "</p>";
