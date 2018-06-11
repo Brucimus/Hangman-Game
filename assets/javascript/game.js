ec98440f994f26bef48cb5593320d930e4fd3d2f
Brucimus <Brucimus@gmail.com>
2018-06-10 20:50:07 -0700

    limitted choices to lower case and capital letters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,5 @@
 var computerChoices = [["s","t","a","r","k"],["t","y","r","e","l","l"],["l","a","n","n","i","s","t","e","r"],["t","a","r","g","a","r","i","a","n"],["g","r","e","y","j","o","y"]]
+var availableUserChoices = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
 var win = 0;
 var losses = 0;
 var guesses = 12;
@@ -11,13 +12,17 @@ var gameOver = true;
 
 document.onkeydown = function(Event) {
     var userGuess = event.key;
-
+    userGuess = userGuess.toLowerCase();
     //chooses new name if game is over
     if (gameOver === true) {
     pickName = Math.floor(Math.random() * computerChoices.length);
     gameOver = false;
     }
     
+    //limits options to letters
+    if (availableUserChoices.includes(userGuess) == false) {
+        return;
+    }
 
     //doesn't push letters to guessed letters list if repeated letter
     nameBlanks = "";
